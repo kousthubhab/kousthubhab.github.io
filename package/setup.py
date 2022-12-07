e05d7c615d0e349e5d0ea56a8329fe4711d52560
kousthubhab <113101952+kousthubhab@users.noreply.github.com>
2022-12-07 12:34:16 +0530

    Add files via upload

---
+++ package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='Package',
+      version='1.0',
+      description='Python functions',
+      author='Kousthubha',
+      author_email='h20220842@pilani.bits-pilani.ac.in',
+      packages=['package'],
+     )
